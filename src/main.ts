d0cf5da52cad5b0f37be1ec78abbcaee7bc1c08a
CentLee <step323@naver.com>
2023-02-25 14:54:48 +0900

    feature: Add Users API Interface

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { UserModule } from 'Users/application/UserModule';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(UserModule);
   await app.listen(3000);
   console.log('Runing on port ==> ', 3000);
 }
